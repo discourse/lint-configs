4b2b8c3eb432881cf3429a9568cf0bb7a9dc06c6
Jarek Radosz <jradosz@gmail.com>
2019-11-01 20:45:22 +0100

    Make ESLint use the rules from index.js
    * Use index.js eslint rules for this repo
    
    * Add eslint and babel-eslint to devDependencies
    
    * Remove eslint-plugin-node from devDependencies
    
    * Remove package-lock.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,1 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-};
+module.exports = require("./index");
