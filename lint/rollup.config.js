2ed6263050a4ce488cfda1ed9db4b958855d683b
Jarek Radosz <jradosz@gmail.com>
2023-10-13 11:52:42 +0200

    Add a rollup config

---
+++ lint/rollup.config.js
@@ -0,0 +1,13 @@
+export default {
+  output: {
+    format: "cjs",
+  },
+  external: [
+    "@babel/eslint-parser",
+    "eslint-plugin-ember",
+    "eslint-plugin-discourse-ember",
+    "eslint-plugin-sort-class-members",
+    "eslint-plugin-decorator-position",
+    "eslint-plugin-simple-import-sort",
+  ],
+};
