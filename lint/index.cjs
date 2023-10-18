7bab46073cd428f4315d615a428d09d2d8a28494
Jarek Radosz <jradosz@gmail.com>
2023-10-19 01:48:26 +0200

    remove index files

--- lint/index.cjs
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-exports.eslint = require("./eslintrc.cjs");
-exports.eslintTheme = require("./eslint-theme.cjs");
-exports.prettier = require("./.prettierrc.cjs");
-exports.templateLint = require("./template-lint.config.cjs");
--- lint/index.js
+++
@@ -1,3 +0,0 @@
-export { default as eslint } from "./-eslint.config.js";
-export { default as eslintTheme } from "./eslint-theme.js";
-export { default as templateLint } from "./template-lint.config.js";
