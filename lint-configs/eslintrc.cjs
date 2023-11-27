d5802c5af24c27ab4a75e7866e01d4161a33dc91
Jarek Radosz <jradosz@gmail.com>
2023-11-27 23:11:26 +0100

    Revert "Try eslint-plugin-ember v12 (alpha)"
    This reverts commit 8c1be099336076d58024249cc1786f4dfd6d29eb.
    
    # Conflicts:
    #	lint-configs/package.json
    #	pnpm-lock.yaml

--- lint-configs/eslintrc.cjs
+++ lint-configs/eslintrc.cjs
@@ -234,11 +234,16 @@ module.exports = {
     ],
   },
 
+  // https://github.com/ember-cli/eslint-plugin-ember/issues/1895
+  // We may eventually be able to drop this by extending the base
+  // config from eslint-plugin-ember. In the meantime, this
   overrides: [
     {
-      files: ["**/*.gjs"],
-      parser: "eslint-plugin-ember/gjs-gts-parser",
-      extends: ["plugin:ember/recommended-gjs"],
+      files: ["**/*.gjs", "**/*.gts"],
+      processor: "ember/<template>",
+      globals: {
+        [TEMPLATE_TAG_PLACEHOLDER]: "readonly",
+      },
     },
   ],
 };
