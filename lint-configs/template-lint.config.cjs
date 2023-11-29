5fdeb7d2db6d37847947c9be1d4cdf4c9e01a81a
Jarek Radosz <jradosz@gmail.com>
2023-11-29 17:52:24 +0100

    DEV: Add new rules, update deps (#52)

--- lint-configs/template-lint.config.cjs
+++ lint-configs/template-lint.config.cjs
@@ -1,33 +1,46 @@
 module.exports = {
   extends: ["recommended", "stylistic"],
   rules: {
-    "no-positive-tabindex": false,
+    // Intentionally disabled default rules
     "no-autofocus-attribute": false,
+    "no-positive-tabindex": false,
     "require-mandatory-role-attributes": false,
     "require-media-caption": false,
+
+    // Pending default rules
+    "link-href-attributes": false,
+    "no-action": false,
+    "no-curly-component-invocation": false,
+    "no-duplicate-landmark-elements": false,
+    "no-implicit-this": false,
+    "no-inline-styles": false,
+    "no-link-to-positional-params": false,
+    "no-link-to-tagname": false,
+    "no-passed-in-event-handlers": false,
+    "no-route-action": false,
+    "require-input-label": false,
+    "require-presentational-children": false,
+    "require-valid-alt-text": false,
+
+    // Non-default rules
+    "no-unnecessary-curly-parens": true,
+    "no-unnecessary-curly-strings": true,
+    "simple-modifiers": true,
+
+    // Pending non-default rules
+    "attribute-order": false,
+    "inline-link-to": false,
+    "no-action-modifiers": false,
+    "no-builtin-form-components": false,
+    "no-this-in-template-only-components": false, // emits false-positives in gjs
 
     // GJS compatibility
     "modifier-name-case": false,
 
-    // Pending
-    "require-presentational-children": false,
-    "no-duplicate-landmark-elements": false,
-    "no-passed-in-event-handlers": false,
-    "no-inline-styles": false,
-    "no-link-to-tagname": false,
-    "no-implicit-this": false,
-    "require-valid-alt-text": false,
-    "link-href-attributes": false,
-    "no-curly-component-invocation": false,
-    "no-link-to-positional-params": false,
-    "require-input-label": false,
-    "no-route-action": false,
-    "no-action": false,
-
     // Prettier compatibility
-    "eol-last": false,
-    "self-closing-void-elements": false,
     "block-indentation": false,
+    "eol-last": false,
     quotes: false,
+    "self-closing-void-elements": false,
   },
 };
