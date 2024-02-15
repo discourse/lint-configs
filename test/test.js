5c90f2f391f24faa95e2bf7b3d71a98021d0249e
Jarek Radosz <jradosz@gmail.com>
2024-02-15 20:17:47 +0100

    DEV: Update dependencies and add a test (#63)

--- test/test.js
+++ test/test.js
@@ -11,6 +11,13 @@ const expectedEslintOutput = `
   1 error and 0 warnings potentially fixable with the \`--fix\` option.
 `;
 
+const expectedEslintDecoratorsOutput = `
+/path-prefix/object.js
+  4:4  error  'computed' is not defined  no-undef
+
+✖ 1 problem (1 error, 0 warnings)
+`;
+
 const expectedTemplateLintOutput = `
 my-component.gjs
   18:4  error  Unexpected {{log}} usage.  no-log
@@ -49,6 +56,27 @@ function eslintAutofix() {
     process.exitCode = 1;
     console.error(`failed\n${e.stdout}`);
     return;
+  }
+}
+
+function eslintObjectDecorators() {
+  stdout.write("eslint - decorators in object literals... ");
+
+  let actual;
+  try {
+    actual = execSync("pnpm eslint object.js").toString();
+  } catch (e) {
+    actual = e.stdout.toString();
+    actual = actual.replace(/^\/.+\/test\/(cjs|cjs-theme)\//m, "/path-prefix/");
+  }
+
+  if (expectedEslintDecoratorsOutput.trim() === actual.trim()) {
+    console.log("✅");
+  } else {
+    process.exitCode = 1;
+    console.error(
+      `failed\n\nexpected:\n${expectedEslintDecoratorsOutput}\nactual:\n${actual}`
+    );
   }
 }
 
@@ -142,6 +170,7 @@ console.log("\ncjs:");
 chdir("cjs");
 eslint();
 eslintAutofix();
+eslintObjectDecorators();
 prettier();
 prettierDecorators();
 prettierScss();
@@ -152,6 +181,7 @@ console.log("\ncjs theme:");
 chdir("cjs-theme");
 eslint();
 eslintAutofix();
+eslintObjectDecorators();
 prettier();
 prettierDecorators();
 prettierScss();
