84dfa8d5cae192da1e05a25efaf4fe7bd3416571
David Taylor <david@taylorhq.com>
2024-11-19 18:53:38 +0000

    tests

--- test/eslint-rules/i18n-t.test.mjs
+++ test/eslint-rules/i18n-t.test.mjs
@@ -10,14 +10,14 @@ ruleTester.run("i18n-t", rule, {
     {
       name: "Simple i18n",
       code: [
-        "import { i18n } from 'discourse-i18n';",
+        'import { i18n } from "discourse-i18n";',
         'i18n("some string");',
       ].join("\n"),
     },
     {
       name: "i18n and I18n.messageFormat",
       code: [
-        "import I18n, { i18n } from 'discourse-i18n';",
+        'import I18n, { i18n } from "discourse-i18n";',
         'i18n("some string");',
         'I18n.messageFormat("some string");',
       ].join("\n"),
@@ -27,7 +27,7 @@ ruleTester.run("i18n-t", rule, {
     {
       name: "Simple I18n.t",
       code: [
-        "import I18n from 'discourse-i18n';",
+        'import I18n from "discourse-i18n";',
         'I18n.t("some string");',
         'I18n.t("some string");',
       ].join("\n"),
@@ -40,7 +40,7 @@ ruleTester.run("i18n-t", rule, {
         },
       ],
       output: [
-        "import { i18n } from 'discourse-i18n';",
+        'import { i18n } from "discourse-i18n";',
         'i18n("some string");',
         'i18n("some string");',
       ].join("\n"),
@@ -48,7 +48,7 @@ ruleTester.run("i18n-t", rule, {
     {
       name: "I18n.t and I18n.messageFormat",
       code: [
-        "import I18n from 'discourse-i18n';",
+        'import I18n from "discourse-i18n";',
         'I18n.t("some string");',
         'I18n.t("some string");',
         'I18n.messsageFormat("some string");',
@@ -62,7 +62,7 @@ ruleTester.run("i18n-t", rule, {
         },
       ],
       output: [
-        "import I18n, { i18n } from 'discourse-i18n';",
+        'import I18n, { i18n } from "discourse-i18n";',
         'i18n("some string");',
         'i18n("some string");',
         'I18n.messsageFormat("some string");',
@@ -71,7 +71,7 @@ ruleTester.run("i18n-t", rule, {
     {
       name: "mix of I18n.t and i18n",
       code: [
-        "import I18n, { i18n } from 'discourse-i18n';",
+        'import I18n, { i18n } from "discourse-i18n";',
         'I18n.t("some string");',
         'i18n("some string");',
       ].join("\n"),
@@ -81,7 +81,7 @@ ruleTester.run("i18n-t", rule, {
         },
       ],
       output: [
-        "import { i18n } from 'discourse-i18n';",
+        'import { i18n } from "discourse-i18n";',
         'i18n("some string");',
         'i18n("some string");',
       ].join("\n"),
