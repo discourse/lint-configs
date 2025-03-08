c0c89e8e38a6c5562ceca5642fcd99bcf8207757
Jarek Radosz <jarek@cvx.dev>
2025-03-08 22:27:32 +0100

    dry

--- lint-configs/eslint-rules/line-after-imports.mjs
+++ lint-configs/eslint-rules/line-after-imports.mjs
@@ -1,9 +1,12 @@
-function isTokenOnSameLine(left, right) {
-  return left?.loc?.end.line === right?.loc?.start.line;
-}
+import {
+  findFirstConsecutiveTokenBefore,
+  findLastConsecutiveTokenAfter,
+  getBoundaryTokens,
+  hasTokenOrCommentBetween,
+} from "./utils/tokens";
 
-function isSemicolonToken(token) {
-  return token.value === ";" && token.type === "Punctuator";
+function findLastIndexOfType(nodes, type) {
+  return nodes.findLastIndex((node) => node.type === type);
 }
 
 export default {
@@ -18,71 +21,6 @@ export default {
 
   create(context) {
     const sourceCode = context.sourceCode;
-
-    function findLastIndexOfType(nodes, type) {
-      return nodes.findLastIndex((node) => node.type === type);
-    }
-
-    function findLastConsecutiveTokenAfter(
-      prevLastToken,
-      nextFirstToken,
-      maxLine
-    ) {
-      const after = sourceCode.getTokenAfter(prevLastToken, {
-        includeComments: true,
-      });
-
-      if (
-        after !== nextFirstToken &&
-        after.loc.start.line - prevLastToken.loc.end.line <= maxLine
-      ) {
-        return findLastConsecutiveTokenAfter(after, nextFirstToken, maxLine);
-      }
-
-      return prevLastToken;
-    }
-
-    function findFirstConsecutiveTokenBefore(
-      nextFirstToken,
-      prevLastToken,
-      maxLine
-    ) {
-      const before = sourceCode.getTokenBefore(nextFirstToken, {
-        includeComments: true,
-      });
-
-      if (
-        before !== prevLastToken &&
-        nextFirstToken.loc.start.line - before.loc.end.line <= maxLine
-      ) {
-        return findFirstConsecutiveTokenBefore(before, prevLastToken, maxLine);
-      }
-
-      return nextFirstToken;
-    }
-
-    function getBoundaryTokens(curNode, nextNode) {
-      const lastToken = sourceCode.getLastToken(curNode);
-      const prevToken = sourceCode.getTokenBefore(lastToken);
-      const nextToken = sourceCode.getFirstToken(nextNode); // skip possible lone `;` between nodes
-
-      const isSemicolonLessStyle =
-        isSemicolonToken(lastToken) &&
-        !isTokenOnSameLine(prevToken, lastToken) &&
-        isTokenOnSameLine(lastToken, nextToken);
-
-      return isSemicolonLessStyle
-        ? { curLast: prevToken, nextFirst: lastToken }
-        : { curLast: lastToken, nextFirst: nextToken };
-    }
-
-    function hasTokenOrCommentBetween(before, after) {
-      return (
-        sourceCode.getTokensBetween(before, after, {
-          includeComments: true,
-        }).length !== 0
-      );
-    }
 
     return {
       Program(node) {
@@ -100,16 +38,19 @@ export default {
         }
 
         const { curLast, nextFirst } = getBoundaryTokens(
+          sourceCode,
           body[index],
           body[index + 1]
         );
 
         const beforePadding = findLastConsecutiveTokenAfter(
+          sourceCode,
           curLast,
           nextFirst,
           1
         );
         const afterPadding = findFirstConsecutiveTokenBefore(
+          sourceCode,
           nextFirst,
           curLast,
           1
@@ -117,10 +58,12 @@ export default {
         const isPadded =
           afterPadding.loc.start.line - beforePadding.loc.end.line > 1;
         const hasTokenInPadding = hasTokenOrCommentBetween(
+          sourceCode,
           beforePadding,
           afterPadding
         );
         const curLineLastToken = findLastConsecutiveTokenAfter(
+          sourceCode,
           curLast,
           nextFirst,
           0
@@ -143,80 +86,6 @@ export default {
           },
         });
       },
-
-      // ImportDeclaration(node) {
-      //   const modulePath = node.source.value.toLowerCase();
-
-      //   // todo
-      // },
-
-      // ClassBody(node) {
-      //   const body = node.body;
-
-      //   for (let i = 0; i < body.length - 1; i++) {
-      //     const curFirst = sourceCode.getFirstToken(body[i]);
-      //     const { curLast, nextFirst } = getBoundaryTokens(
-      //       body[i],
-      //       body[i + 1]
-      //     );
-      //     const singleLine = isTokenOnSameLine(curFirst, curLast);
-      //     const skip =
-      //       singleLine && nodeType(body[i]) === nodeType(body[i + 1]);
-      //     const beforePadding = findLastConsecutiveTokenAfter(
-      //       curLast,
-      //       nextFirst,
-      //       1
-      //     );
-      //     const afterPadding = findFirstConsecutiveTokenBefore(
-      //       nextFirst,
-      //       curLast,
-      //       1
-      //     );
-      //     const isPadded =
-      //       afterPadding.loc.start.line - beforePadding.loc.end.line > 1;
-      //     const hasTokenInPadding = hasTokenOrCommentBetween(
-      //       beforePadding,
-      //       afterPadding
-      //     );
-      //     const curLineLastToken = findLastConsecutiveTokenAfter(
-      //       curLast,
-      //       nextFirst,
-      //       0
-      //     );
-      //     const paddingType = getPaddingType(body[i], body[i + 1]);
-
-      //     if (paddingType === "never" && isPadded) {
-      //       context.report({
-      //         node: body[i + 1],
-      //         messageId: "never",
-
-      //         fix(fixer) {
-      //           if (hasTokenInPadding) {
-      //             return null;
-      //           }
-
-      //           return fixer.replaceTextRange(
-      //             [beforePadding.range[1], afterPadding.range[0]],
-      //             "\n"
-      //           );
-      //         },
-      //       });
-      //     } else if (paddingType === "always" && !skip && !isPadded) {
-      //       context.report({
-      //         node: body[i + 1],
-      //         messageId: "always",
-
-      //         fix(fixer) {
-      //           if (hasTokenInPadding) {
-      //             return null;
-      //           }
-
-      //           return fixer.insertTextAfter(curLineLastToken, "\n");
-      //         },
-      //       });
-      //     }
-      //   }
-      // },
     };
   },
 };
