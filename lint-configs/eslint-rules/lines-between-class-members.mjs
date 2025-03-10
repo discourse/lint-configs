53fbdbc130c124b04b460b2bfa5cafea35d6d4b9
Jarek Radosz <jradosz@gmail.com>
2025-03-10 11:39:24 +0100

    FEATURE: Add `line-after-imports` rule (#105)
    Ensures a blank line after the last `import`:
    
    invalid:
    
    ```js
    import x from "foo";
    const xCopy = x;
    import z from "bar";
    const STRING = "hey";
    // test
    class Y {}
    ```
    
    valid:
    
    ```js
    import x from "foo";
    const xCopy = x;
    import z from "bar";
    
    const STRING = "hey";
    // test
    class Y {}
    ```

--- lint-configs/eslint-rules/lines-between-class-members.mjs
+++ lint-configs/eslint-rules/lines-between-class-members.mjs
@@ -1,10 +1,13 @@
-function isTokenOnSameLine(left, right) {
-  return left?.loc?.end.line === right?.loc?.start.line;
-}
+// Based on `@stylistic/js/lines-between-class-members`
+// See: https://github.com/eslint-stylistic/eslint-stylistic/blob/d6809c910510a4477e01ea248071f0701d0af4ed/packages/eslint-plugin/rules/lines-between-class-members/lines-between-class-members._js_.ts
 
-function isSemicolonToken(token) {
-  return token.value === ";" && token.type === "Punctuator";
-}
+import {
+  findFirstConsecutiveTokenBefore,
+  findLastConsecutiveTokenAfter,
+  getBoundaryTokens,
+  hasTokenOrCommentBetween,
+  isTokenOnSameLine,
+} from "./utils/tokens.mjs";
 
 export default {
   meta: {
@@ -28,115 +31,6 @@ export default {
       { blankLine: "always", prev: "*", next: "template" },
     ];
     const sourceCode = context.sourceCode;
-
-    /**
-     * Gets a pair of tokens that should be used to check lines between two class member nodes.
-     *
-     * In most cases, this returns the very last token of the current node and
-     * the very first token of the next node.
-     * For example:
-     *
-     *     class C {
-     *         x = 1;   // curLast: `;` nextFirst: `in`
-     *         in = 2
-     *     }
-     *
-     * There is only one exception. If the given node ends with a semicolon, and it looks like
-     * a semicolon-less style's semicolon - one that is not on the same line as the preceding
-     * token, but is on the line where the next class member starts - this returns the preceding
-     * token and the semicolon as boundary tokens.
-     * For example:
-     *
-     *     class C {
-     *         x = 1    // curLast: `1` nextFirst: `;`
-     *         ;in = 2
-     *     }
-     * When determining the desired layout of the code, we should treat this semicolon as
-     * a part of the next class member node instead of the one it technically belongs to.
-     * @param curNode Current class member node.
-     * @param nextNode Next class member node.
-     * @returns The actual last token of `node`.
-     * @private
-     */
-    function getBoundaryTokens(curNode, nextNode) {
-      const lastToken = sourceCode.getLastToken(curNode);
-      const prevToken = sourceCode.getTokenBefore(lastToken);
-      const nextToken = sourceCode.getFirstToken(nextNode); // skip possible lone `;` between nodes
-
-      const isSemicolonLessStyle =
-        isSemicolonToken(lastToken) &&
-        !isTokenOnSameLine(prevToken, lastToken) &&
-        isTokenOnSameLine(lastToken, nextToken);
-
-      return isSemicolonLessStyle
-        ? { curLast: prevToken, nextFirst: lastToken }
-        : { curLast: lastToken, nextFirst: nextToken };
-    }
-
-    /**
-     * Return the last token among the consecutive tokens that have no exceed max line difference in between, before the first token in the next member.
-     * @param prevLastToken The last token in the previous member node.
-     * @param nextFirstToken The first token in the next member node.
-     * @param maxLine The maximum number of allowed line difference between consecutive tokens.
-     * @returns  The last token among the consecutive tokens.
-     */
-    function findLastConsecutiveTokenAfter(
-      prevLastToken,
-      nextFirstToken,
-      maxLine
-    ) {
-      const after = sourceCode.getTokenAfter(prevLastToken, {
-        includeComments: true,
-      });
-
-      if (
-        after !== nextFirstToken &&
-        after.loc.start.line - prevLastToken.loc.end.line <= maxLine
-      ) {
-        return findLastConsecutiveTokenAfter(after, nextFirstToken, maxLine);
-      }
-
-      return prevLastToken;
-    }
-
-    /**
-     * Return the first token among the consecutive tokens that have no exceed max line difference in between, after the last token in the previous member.
-     * @param nextFirstToken The first token in the next member node.
-     * @param prevLastToken The last token in the previous member node.
-     * @param maxLine The maximum number of allowed line difference between consecutive tokens.
-     * @returns The first token among the consecutive tokens.
-     */
-    function findFirstConsecutiveTokenBefore(
-      nextFirstToken,
-      prevLastToken,
-      maxLine
-    ) {
-      const before = sourceCode.getTokenBefore(nextFirstToken, {
-        includeComments: true,
-      });
-
-      if (
-        before !== prevLastToken &&
-        nextFirstToken.loc.start.line - before.loc.end.line <= maxLine
-      ) {
-        return findFirstConsecutiveTokenBefore(before, prevLastToken, maxLine);
-      }
-
-      return nextFirstToken;
-    }
-
-    /**
-     * Checks if there is a token or comment between two tokens.
-     * @param before The token before.
-     * @param after The token after.
-     * @returns True if there is a token or comment between two tokens.
-     */
-    function hasTokenOrCommentBetween(before, after) {
-      return (
-        sourceCode.getTokensBetween(before, after, { includeComments: true })
-          .length !== 0
-      );
-    }
 
     /**
      * Returns the type of the node.
@@ -206,6 +100,7 @@ export default {
         for (let i = 0; i < body.length - 1; i++) {
           const curFirst = sourceCode.getFirstToken(body[i]);
           const { curLast, nextFirst } = getBoundaryTokens(
+            sourceCode,
             body[i],
             body[i + 1]
           );
@@ -213,11 +108,13 @@ export default {
           const skip =
             singleLine && nodeType(body[i]) === nodeType(body[i + 1]);
           const beforePadding = findLastConsecutiveTokenAfter(
+            sourceCode,
             curLast,
             nextFirst,
             1
           );
           const afterPadding = findFirstConsecutiveTokenBefore(
+            sourceCode,
             nextFirst,
             curLast,
             1
@@ -225,10 +122,12 @@ export default {
           const isPadded =
             afterPadding.loc.start.line - beforePadding.loc.end.line > 1;
           const hasTokenInPadding = hasTokenOrCommentBetween(
+            sourceCode,
             beforePadding,
             afterPadding
           );
           const curLineLastToken = findLastConsecutiveTokenAfter(
+            sourceCode,
             curLast,
             nextFirst,
             0
---
+++ test/eslint-rules/line-after-imports.test.mjs
@@ -0,0 +1,56 @@
+import { RuleTester } from "eslint";
+import rule from "../../lint-configs/eslint-rules/line-after-imports.mjs";
+
+const ruleTester = new RuleTester({
+  languageOptions: { ecmaVersion: 2018 },
+});
+
+ruleTester.run("line-after-imports", rule, {
+  valid: [
+    {
+      code: `
+        import x from "foo";
+
+        class Y {}
+      `,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+        import x from "foo";
+        import z from "bar";
+        // test
+        class Y {}
+      `,
+      errors: [{ message: "Expected blank line after imports." }],
+      output: `
+        import x from "foo";
+        import z from "bar";
+
+        // test
+        class Y {}
+      `,
+    },
+    {
+      code: `
+        import x from "foo";
+        const xCopy = x;
+        import z from "bar";
+        const STRING = "hey";
+        // test
+        class Y {}
+      `,
+      errors: [{ message: "Expected blank line after imports." }],
+      output: `
+        import x from "foo";
+        const xCopy = x;
+        import z from "bar";
+
+        const STRING = "hey";
+        // test
+        class Y {}
+      `,
+    },
+  ],
+});
