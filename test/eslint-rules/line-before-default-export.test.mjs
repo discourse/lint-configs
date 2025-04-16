c6bf2c52e7e3f3ecdcdc67ad910d4a0fc1240d5d
Jarek Radosz <jradosz@gmail.com>
2025-04-16 11:20:55 +0200

    FEATURE: Add a blank line before default exports (#114)

---
+++ test/eslint-rules/line-before-default-export.test.mjs
@@ -0,0 +1,39 @@
+import EmberESLintParser from "ember-eslint-parser";
+import { RuleTester } from "eslint";
+import rule from "../../lint-configs/eslint-rules/line-before-default-export.mjs";
+
+const ruleTester = new RuleTester({
+  languageOptions: { parser: EmberESLintParser },
+});
+
+ruleTester.run("line-before-default-export", rule, {
+  valid: [
+    {
+      code: `
+        const Foo = <template>
+          test
+        </template>;
+
+        export default Foo;
+      `,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+        const Foo = <template>
+          test
+        </template>;
+        export default Foo;
+      `,
+      errors: [{ message: "Expected blank line before the default export." }],
+      output: `
+        const Foo = <template>
+          test
+        </template>;
+
+        export default Foo;
+      `,
+    },
+  ],
+});
